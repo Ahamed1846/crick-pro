258e4d27346be8f7ab74408721e5f8641a676216
Ahamed1846 <116168645+Ahamed1846@users.noreply.github.com>
2022-11-23 17:27:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# crick-pro
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-cm2g83)
---
+++ app.js
@@ -0,0 +1,92 @@
+var batsman1 = document.getElementById('batsman1');
+var batsman2 = document.getElementById('batsman2');
+var batsman3 = document.getElementById('batsman3');
+var batsman4 = document.getElementById('batsman4');
+var batsman5 = document.getElementById('batsman5');
+var batsman6 = document.getElementById('batsman6');
+var batsman7 = document.getElementById('batsman7');
+var batsman8 = document.getElementById('batsman8');
+var batsman9 = document.getElementById('batsman9');
+var batsman10 = document.getElementById('batsman10');
+var batsman11 = document.getElementById('batsman11');
+
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+var img4 = document.getElementById('img4');
+var img5 = document.getElementById('img5');
+var img6 = document.getElementById('img6');
+var img7 = document.getElementById('img7');
+var img8 = document.getElementById('img8');
+var img9 = document.getElementById('img9');
+var img10 = document.getElementById('img10');
+var img11 = document.getElementById('img11');
+
+batsman1.onclick = () => {
+  img1.style.display = 'block';
+};
+batsman2.onclick = () => {
+  img2.style.display = 'block';
+};
+batsman3.onclick = () => {
+  img3.style.display = 'block';
+};
+batsman4.onclick = () => {
+  img4.style.display = 'block';
+};
+batsman5.onclick = () => {
+  img5.style.display = 'block';
+};
+batsman6.onclick = () => {
+  img6.style.display = 'block';
+};
+batsman7.onclick = () => {
+  img7.style.display = 'block';
+};
+batsman8.onclick = () => {
+  img8.style.display = 'block';
+};
+batsman9.onclick = () => {
+  img9.style.display = 'block';
+};
+batsman10.onclick = () => {
+  img10.style.display = 'block';
+};
+batsman11.onclick = () => {
+  img11.style.display = 'block';
+};
+
+img1.onclick = () => {
+  img1.style.display = 'none';
+};
+img2.onclick = () => {
+  img2.style.display = 'none';
+};
+img3.onclick = () => {
+  img3.style.display = 'none';
+};
+img4.onclick = () => {
+  img4.style.display = 'none';
+};
+img5.onclick = () => {
+  img5.style.display = 'none';
+};
+img6.onclick = () => {
+  img6.style.display = 'none';
+};
+img7.onclick = () => {
+  img7.style.display = 'none';
+};
+img8.onclick = () => {
+  img8.style.display = 'none';
+};
+
+img9.onclick = () => {
+  img9.style.display = 'none';
+};
+img10.onclick = () => {
+  img10.style.display = 'none';
+};
+img11.onclick = () => {
+  img11.style.display = 'none';
+};
